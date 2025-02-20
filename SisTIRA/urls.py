dd4eefda938642089f1567047a0e38b040966f66
avelando <avelarrodrigues89@gmail.com>
2025-02-20 14:16:53 -0300

    Alterando a rota de admin

--- SisTIRA/urls.py
+++ SisTIRA/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('api/accounts/', include('accounts.urls')),
     path('api/questions/', include('questions.urls')),
